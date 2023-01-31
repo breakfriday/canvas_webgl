da110e8db10bce1bca9dfcd62c6cf60f16f7c3ed
breakhappy <break_happy@163.com>
2023-01-31 19:53:36 +0800

    feat:update

--- packages/reactCanvas/core/canvas.ts
+++ packages/reactCanvas/core/canvas.ts
@@ -1,4 +1,35 @@
 
+let _pixelRatio;
+
+const createCanvasElement = () => {
+  const canvas: HTMLCanvasElement = document.createElement('canvas');
+
+  return canvas;
+};
+
+function getDevicePixelRatio() {
+  if (_pixelRatio) {
+    return _pixelRatio;
+  }
+  const canvas = createCanvasElement();
+  const context = canvas.getContext('2d') as any;
+  _pixelRatio = (function () {
+    const devicePixelRatio = window.devicePixelRatio || 1;
+    const backingStoreRatio =
+        context.webkitBackingStorePixelRatio ||
+        context.mozBackingStorePixelRatio ||
+        context.msBackingStorePixelRatio ||
+        context.oBackingStorePixelRatio ||
+        context.backingStorePixelRatio ||
+        1;
+    return devicePixelRatio / backingStoreRatio;
+  })();
+
+  canvas.width = 0;
+  canvas.height = 0;
+  return _pixelRatio;
+}
+
 
 class Canvas {
   private _canvas?: HTMLCanvasElement;
