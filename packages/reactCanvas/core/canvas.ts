425ae1bc3317aaa1fec6292e99e9dd68ebb5714c
breakhappy <break_happy@163.com>
2023-01-31 18:37:17 +0800

    feat:update

--- packages/reactCanvas/core/canvas.ts
+++ packages/reactCanvas/core/canvas.ts
@@ -35,5 +35,4 @@ class Canvas {
 }
 
 
-
-export default Canvas
+export default Canvas;
--- packages/sheetOne/src/routes.ts
+++ packages/sheetOne/src/routes.ts
@@ -2,6 +2,7 @@ import { IRouterConfig, lazy } from 'ice';
 
 const Home = lazy(() => import('@/pages/Home'));
 const Demo1 = lazy(() => import('@/pages/demo1'));
+// const Demo2 = lazy(() => import('@/pages/demo2'));
 
 const routerConfig: IRouterConfig[] = [
   {
@@ -12,6 +13,10 @@ const routerConfig: IRouterConfig[] = [
     path: '/demo1',
     component: Demo1,
   },
+  // {
+  //   path: '/demo2',
+  //   component: Demo2,
+  // },
 ];
 
 export default routerConfig;
