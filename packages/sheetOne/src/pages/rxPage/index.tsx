0b1cfd2526b86c1df29758cc1fd1163bfb67c823
breakhappy <break_happy@163.com>
2023-02-07 17:14:36 +0800

    feat:update

--- packages/sheetOne/src/pages/rxPage/index.tsx
+++ packages/sheetOne/src/pages/rxPage/index.tsx
@@ -4,7 +4,7 @@ import { Subject, interval, fromEvent, of, from, defer } from 'rxjs';
 import { map, scan, startWith, switchMap, takeUntil, takeWhile, mergeMap, delay } from 'rxjs/operators';
 
 const taskSubject = new Subject();
-const concurrentLimit = 1;
+const concurrentLimit = 2;
 
 const source = taskSubject.pipe(
   mergeMap((task) => defer(() => from(task())), concurrentLimit),
@@ -19,8 +19,22 @@ const addTask = (task) => {
 const Elevator = () => {
   return (
     <div onClick={() => {
-      addTask(() => new Promise((resolve) => setTimeout(() => resolve({ data: 'request 1' }), 3000)));
-      addTask(() => new Promise((resolve) => setTimeout(() => resolve({ data: 'request 2' }), 1000)));
+      addTask(() => {
+        console.log('task1');
+        return new Promise((res) => {
+          setTimeout(() => {
+            res({ data: 'request 1' });
+          }, 3000);
+        });
+      });
+      addTask(() => {
+        console.log('task2');
+        return new Promise((res) => {
+          setTimeout(() => {
+            res({ data: 'request 2' });
+          }, 3000);
+        });
+      });
       addTask(() => new Promise((resolve) => setTimeout(() => resolve({ data: 'request 3' }), 100)));
       addTask(() => new Promise((resolve) => setTimeout(() => resolve({ data: 'request 4' }), 1000)));
     }}
@@ -33,3 +47,4 @@ const Elevator = () => {
 };
 
 export default Elevator;
+
