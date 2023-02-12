18df2965e5a394d0e0e6b19d67880520dcf128f9
breakhappy <break_happy@163.com>
2023-02-12 16:40:19 +0800

    feat:update

--- packages/sheetOne/src/pages/sheetPage2/index.tsx
+++ packages/sheetOne/src/pages/sheetPage2/index.tsx
@@ -1,10 +1,13 @@
 import React, { useRef } from 'react';
 import Konva from 'konva';
 import { Stage, Layer, Rect, Text } from 'react-konva';
+import SpreadSheet from 'konvaGrid';
 
 const SheetPage2 = () => {
   return (
     <>
+      <SpreadSheet autoFocus={false} />
+
       <Stage width={window.innerWidth} height={window.innerHeight}>
         <Layer>
           <Text text="Try click on rect" />
