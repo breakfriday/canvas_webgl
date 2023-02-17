04ec3892642515d6d923358dac2dc3c3e903a5ed
breakhappy <break_happy@163.com>
2023-02-18 01:59:44 +0800

    feat:update

---
+++ packages/sheetOne/src/pages/complexLoad/index.tsx
@@ -0,0 +1,77 @@
+import Konva from 'konva';
+import React, { useRef, useEffect, useState } from 'react';
+import { Stage, Layer, Rect, Text } from 'react-konva';
+
+
+const possibleFilters = ['', 'blur', 'invert'];
+
+function createObject(attrs) {
+  return Object.assign({}, attrs, {
+    // define position
+    x: 0,
+    y: 0,
+    // here should be url to image
+    src: '',
+    // and define filter on it, let's define that we can have only
+    // "blur", "invert" or "" (none)
+    filter: 'blur',
+  });
+}
+function createYoda(attrs) {
+  return Object.assign(createObject(attrs), {
+    src: '/assets/yoda.jpg',
+  });
+}
+
+function createDarth(attrs) {
+  return Object.assign(createObject(attrs), {
+    src: '/assets/darth-vader.jpg',
+  });
+}
+
+const Complex_Load = () => {
+  const [stage_history, set_stage_history] = useState([
+    {
+      x: 20,
+      y: 20,
+    },
+  ]);
+
+  const [cur_history_step, set_cur_history_step] = useState(0);
+
+  const position = stage_history[cur_history_step];
+
+  const undo_handle = () => {
+
+  };
+
+  const redo_handle = () => {
+
+  };
+
+  const handle_drag_end = () => {
+
+  };
+
+  return (
+    <Stage width={window.innerWidth} height={window.innerHeight}>
+
+      <Layer>
+        <Text text="undo" onClick={undo_handle} />
+        <Text text="redo" x={40} onClick={redo_handle} />
+        <Rect
+          x={position.x}
+          y={position.y}
+          width={50}
+          height={50}
+          fill="black"
+          draggable
+          onDragEnd={handle_drag_end}
+        />
+      </Layer>
+
+    </Stage>
+  );
+};
+
+export default Complex_Load;
