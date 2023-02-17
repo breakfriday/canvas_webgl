ee0d8cb15b71284b56b8279c4354af85e6dedcb8
breakhappy <break_happy@163.com>
2023-02-18 01:06:27 +0800

    feat:update

---
+++ packages/editorCore/src/components/image_editor.tsx
@@ -0,0 +1,18 @@
+import React, { useEffect } from 'react';
+import Konva from 'konva/lib/Core';
+
+import 'konva/lib/shapes/Image';
+import 'konva/lib/shapes/Circle';
+import 'konva/lib/shapes/Rect';
+import 'konva/lib/shapes/Path';
+import 'konva/lib/Animation';
+import 'konva/lib/DragAndDrop';
+
+
+const Image_Editor = (props: any) => {
+  useEffect(() => {
+
+  }, []);
+};
+
+export default Image_Editor;
