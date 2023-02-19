0c35ec74de789a739173055d78413768cb478352
breakhappy <break_happy@163.com>
2023-02-19 19:25:09 +0800

    feat:update

--- packages/sheetOne/src/pages/slateDemo/index.tsx
+++ packages/sheetOne/src/pages/slateDemo/index.tsx
@@ -1,34 +1,50 @@
-import React, { useCallback, useMemo } from 'react'
-import isHotkey from 'is-hotkey'
-import { Editable, withReact, useSlate, Slate } from 'slate-react'
+import React, { useCallback, useMemo } from 'react';
+import isHotkey from 'is-hotkey';
+import { Editable, withReact, useSlate, Slate } from 'slate-react';
 import {
   Editor,
   Transforms,
   createEditor,
   Descendant,
   Element as SlateElement,
-} from 'slate'
-import { withHistory } from 'slate-history'
+} from 'slate';
+import { withHistory } from 'slate-history';
 
-import { Button, Icon, Toolbar } from './components/index'
+import { Button, Icon, Toolbar } from './components/index';
 
 const HOTKEYS = {
   'mod+b': 'bold',
   'mod+i': 'italic',
   'mod+u': 'underline',
   'mod+`': 'code',
-}
+};
 
-const LIST_TYPES = ['numbered-list', 'bulleted-list']
-const TEXT_ALIGN_TYPES = ['left', 'center', 'right', 'justify']
+const LIST_TYPES = ['numbered-list', 'bulleted-list'];
+const TEXT_ALIGN_TYPES = ['left', 'center', 'right', 'justify'];
 
 const RichTextExample = () => {
-  const renderElement = useCallback(props => <Element {...props} />, [])
-  const renderLeaf = useCallback(props => <Leaf {...props} />, [])
-  const editor = useMemo(() => withHistory(withReact(createEditor())), [])
+  const renderElement = useCallback((props) => <Element {...props} />, []);
+  const renderLeaf = useCallback((props) => <Leaf {...props} />, []);
+  const editor = useMemo(() => withHistory(withReact(createEditor())), []);
 
   return (
-    <Slate editor={editor} value={initialValue}>
+    <Slate
+      editor={editor}
+      value={initialValue}
+      onChange={(value) => {
+        const isAstChange = editor.operations.some(
+          (op) => op.type !== 'set_selection',
+        );
+
+        if (isAstChange) {
+          console.info(editor.operations);
+          // Save the value to Local Storage.
+          const content = JSON.stringify(value);
+          console.info(value)
+          localStorage.setItem('content', content);
+        }
+      }}
+    >
       <Toolbar>
         <MarkButton format="bold" icon="format_bold" />
         <MarkButton format="italic" icon="format_italic" />
@@ -50,187 +66,187 @@ const RichTextExample = () => {
         placeholder="Enter some rich text…"
         spellCheck
         autoFocus
-        onKeyDown={event => {
+        onKeyDown={(event) => {
           for (const hotkey in HOTKEYS) {
             if (isHotkey(hotkey, event as any)) {
-              event.preventDefault()
-              const mark = HOTKEYS[hotkey]
-              toggleMark(editor, mark)
+              event.preventDefault();
+              const mark = HOTKEYS[hotkey];
+              toggleMark(editor, mark);
             }
           }
         }}
       />
     </Slate>
-  )
-}
+  );
+};
 
 const toggleBlock = (editor, format) => {
   const isActive = isBlockActive(
     editor,
     format,
-    TEXT_ALIGN_TYPES.includes(format) ? 'align' : 'type'
-  )
-  const isList = LIST_TYPES.includes(format)
+    TEXT_ALIGN_TYPES.includes(format) ? 'align' : 'type',
+  );
+  const isList = LIST_TYPES.includes(format);
 
   Transforms.unwrapNodes(editor, {
-    match: n =>
+    match: (n) =>
       !Editor.isEditor(n) &&
       SlateElement.isElement(n) &&
       LIST_TYPES.includes(n.type) &&
       !TEXT_ALIGN_TYPES.includes(format),
     split: true,
-  })
-  let newProperties: Partial<SlateElement>
+  });
+  let newProperties: Partial<SlateElement>;
   if (TEXT_ALIGN_TYPES.includes(format)) {
     newProperties = {
       align: isActive ? undefined : format,
-    }
+    };
   } else {
     newProperties = {
       type: isActive ? 'paragraph' : isList ? 'list-item' : format,
-    }
+    };
   }
-  Transforms.setNodes<SlateElement>(editor, newProperties)
+  Transforms.setNodes<SlateElement>(editor, newProperties);
 
   if (!isActive && isList) {
-    const block = { type: format, children: [] }
-    Transforms.wrapNodes(editor, block)
+    const block = { type: format, children: [] };
+    Transforms.wrapNodes(editor, block);
   }
-}
+};
 
 const toggleMark = (editor, format) => {
-  const isActive = isMarkActive(editor, format)
+  const isActive = isMarkActive(editor, format);
 
   if (isActive) {
-    Editor.removeMark(editor, format)
+    Editor.removeMark(editor, format);
   } else {
-    Editor.addMark(editor, format, true)
+    Editor.addMark(editor, format, true);
   }
-}
+};
 
 const isBlockActive = (editor, format, blockType = 'type') => {
-  const { selection } = editor
-  if (!selection) return false
+  const { selection } = editor;
+  if (!selection) return false;
 
   const [match] = Array.from(
     Editor.nodes(editor, {
       at: Editor.unhangRange(editor, selection),
-      match: n =>
+      match: (n) =>
         !Editor.isEditor(n) &&
         SlateElement.isElement(n) &&
         n[blockType] === format,
-    })
-  )
+    }),
+  );
 
-  return !!match
-}
+  return !!match;
+};
 
 const isMarkActive = (editor, format) => {
-  const marks = Editor.marks(editor)
-  return marks ? marks[format] === true : false
-}
+  const marks = Editor.marks(editor);
+  return marks ? marks[format] === true : false;
+};
 
 const Element = ({ attributes, children, element }) => {
-  const style = { textAlign: element.align }
+  const style = { textAlign: element.align };
   switch (element.type) {
     case 'block-quote':
       return (
         <blockquote style={style} {...attributes}>
           {children}
         </blockquote>
-      )
+      );
     case 'bulleted-list':
       return (
         <ul style={style} {...attributes}>
           {children}
         </ul>
-      )
+      );
     case 'heading-one':
       return (
         <h1 style={style} {...attributes}>
           {children}
         </h1>
-      )
+      );
     case 'heading-two':
       return (
         <h2 style={style} {...attributes}>
           {children}
         </h2>
-      )
+      );
     case 'list-item':
       return (
         <li style={style} {...attributes}>
           {children}
         </li>
-      )
+      );
     case 'numbered-list':
       return (
         <ol style={style} {...attributes}>
           {children}
         </ol>
-      )
+      );
     default:
       return (
         <p style={style} {...attributes}>
           {children}
         </p>
-      )
+      );
   }
-}
+};
 
 const Leaf = ({ attributes, children, leaf }) => {
   if (leaf.bold) {
-    children = <strong>{children}</strong>
+    children = <strong>{children}</strong>;
   }
 
   if (leaf.code) {
-    children = <code>{children}</code>
+    children = <code>{children}</code>;
   }
 
   if (leaf.italic) {
-    children = <em>{children}</em>
+    children = <em>{children}</em>;
   }
 
   if (leaf.underline) {
-    children = <u>{children}</u>
+    children = <u>{children}</u>;
   }
 
-  return <span {...attributes}>{children}</span>
-}
+  return <span {...attributes}>{children}</span>;
+};
 
 const BlockButton = ({ format, icon }) => {
-  const editor = useSlate()
+  const editor = useSlate();
   return (
     <Button
       active={isBlockActive(
         editor,
         format,
-        TEXT_ALIGN_TYPES.includes(format) ? 'align' : 'type'
+        TEXT_ALIGN_TYPES.includes(format) ? 'align' : 'type',
       )}
-      onMouseDown={event => {
-        event.preventDefault()
-        toggleBlock(editor, format)
+      onMouseDown={(event) => {
+        event.preventDefault();
+        toggleBlock(editor, format);
       }}
     >
       <Icon>{icon}</Icon>
     </Button>
-  )
-}
+  );
+};
 
 const MarkButton = ({ format, icon }) => {
-  const editor = useSlate()
+  const editor = useSlate();
   return (
     <Button
       active={isMarkActive(editor, format)}
-      onMouseDown={event => {
-        event.preventDefault()
-        toggleMark(editor, format)
+      onMouseDown={(event) => {
+        event.preventDefault();
+        toggleMark(editor, format);
       }}
     >
       <Icon>{icon}</Icon>
     </Button>
-  )
-}
+  );
+};
 
 const initialValue: Descendant[] = [
   {
@@ -268,6 +284,6 @@ const initialValue: Descendant[] = [
     align: 'center',
     children: [{ text: 'Try it out for yourself!' }],
   },
-]
+];
 
-export default RichTextExample
+export default RichTextExample;
