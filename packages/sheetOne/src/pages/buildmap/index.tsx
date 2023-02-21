8fcbed43699dbc85fa621b7c602b3812b530e75d
breakhappy <break_happy@163.com>
2023-02-22 02:20:06 +0800

    feat:update

--- packages/sheetOne/src/pages/buildmap/index.tsx
+++ packages/sheetOne/src/pages/buildmap/index.tsx
@@ -29,7 +29,7 @@ function ThreeScene() {
     // Init scene
 
     const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(55, window.innerWidth / window.innerHeight, 0.9, 1000);
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.9, 1000);
     camera.position.z = 5;
 
     const renderer = new THREE.WebGLRenderer();
