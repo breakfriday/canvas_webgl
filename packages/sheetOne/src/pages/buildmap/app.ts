4e778389c6b49e0c6ee8e780cf905cd7a8e8b3d5
breakhappy <break_happy@163.com>
2023-03-04 13:52:57 +0800

    feat:update

--- packages/sheetOne/src/pages/buildmap/app.ts
+++ packages/sheetOne/src/pages/buildmap/app.ts
@@ -2,8 +2,20 @@
 import * as THREE from 'three';
 import { Scene } from './scense';
 import { Camera } from './camera';
+import { Cube } from './shapes/cube';
+
+
+function Renderer(target: any) {
+  target.prototype.createRenderer = function () {
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+  };
+}
 
 @Scene
+@Camera
+@Cube
+@Renderer
 class App {
   private scene!: THREE.Scene;
   private camera!: THREE.PerspectiveCamera;
@@ -13,7 +25,8 @@ class App {
   }
 
   render() {
-
+    const { scene, camera } = this;
+    this.renderer.render(scene, camera);
   }
 }
 
