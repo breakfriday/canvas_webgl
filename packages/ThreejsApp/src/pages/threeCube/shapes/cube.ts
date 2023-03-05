f80e8e680237451601da44e1e77552736fbfd8e0
breakhappy <break_happy@163.com>
2023-03-05 14:19:24 +0800

    feat:update

--- packages/ThreejsApp/src/pages/threeCube/shapes/cube.ts
+++ packages/ThreejsApp/src/pages/threeCube/shapes/cube.ts
@@ -12,7 +12,7 @@ export function AddCube(size = 1, position: THREE.Vector3 = new THREE.Vector3(0,
         this.addCube();
       }
 
-      addCube() {
+      addCube(position: THREE.Vector3 = new THREE.Vector3(0, 0.5, 0)) {
         const geometry = new THREE.BoxGeometry(1, 1, 1, 10, 10, 10);
         // const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 
@@ -23,7 +23,8 @@ export function AddCube(size = 1, position: THREE.Vector3 = new THREE.Vector3(0,
         });
 
         const cube = new THREE.Mesh(geometry, material);
-        cube.position.set(2, 0, 0);
+        // cube.position.set(0, 0, 0);
+        cube.position.copy(position)
 
         this.cube = cube;
 
--- packages/ThreejsApp/src/pages/threeCube/shapes/gridHelper.ts
+++ packages/ThreejsApp/src/pages/threeCube/shapes/gridHelper.ts
@@ -6,8 +6,8 @@ export function AddGridHelp() {
       constructor(...args: any[]) {
         super(...args);
         const gridHelper = new THREE.GridHelper(100, 90, 0x0000ff, 0x808080);
-        gridHelper.position.y = -0.5;
-        gridHelper.position.x = -1;
+        gridHelper.position.y = 0;
+        gridHelper.position.x = 0;
         this.scene.add(gridHelper);
       }
     };
