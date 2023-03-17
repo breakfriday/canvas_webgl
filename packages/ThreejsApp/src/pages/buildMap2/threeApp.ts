25f1ac64f9838a3196d3c9c9862e5d0a6db85080
break_happy <break_happy@163.com>
2023-03-17 13:47:07 +0800

    feat:update

--- packages/ThreejsApp/src/pages/buildMap2/threeApp.ts
+++ packages/ThreejsApp/src/pages/buildMap2/threeApp.ts
@@ -28,6 +28,7 @@ class threeApp {
   init() {
     this.add_scene();
     this.add_light();
+    this.add_gridHelper();
   }
   // 注册场景
   add_scene() {
@@ -48,6 +49,17 @@ class threeApp {
     this.scene.add(light0);
     this.scene.add(light1);
     this.scene.add(light2);
+  }
+  add_gridHelper() {
+    const gridHelper = new THREE.GridHelper(60, 160, new THREE.Color(0x555555), new THREE.Color(0x333333));
+    this.scene.add(gridHelper);
+  }
+  add_controller() {
+    this.controls = new MapControls(this.camera, this.renderer.domElement);
+    this.controls.enableDamping = true;
+    this.controls.dampingFactor = 0.25;
+    this.controls.screenSpacePanning = false;
+    this.controls.maxDistance = 800;
   }
   // 注册摄像头
   add_camera() {
